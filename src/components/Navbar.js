b7f541749ea2c30af1f6a04f464a6118ac2aa72a
RomanVelnar <roman.velnar@gmail.com>
2022-02-19 12:44:00 +0000

    first commit, initialised create-react-app and cleaned the content, added navbar and custom CSS properties

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navbar() {
+    
+    return(
+        <nav>
+            <ul className='navbar--list'>
+                <li className='navbar--list--item'>Home</li>
+                <li className='navbar--list--item'>About</li>
+                <li className='navbar--list--item'>Services</li>
+                <li className='navbar--list--item'>Contact</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
